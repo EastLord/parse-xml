857a7e73f97316cd5219d0c79ad1b720959e9d92
qiutiandong <qiutiandong@gmail.com>
2017-04-07 08:41:30 +0800

    update shiyan

---
+++ src/shiyan/shiyan1_1.java
@@ -0,0 +1,72 @@
+package shiyan;
+
+/**
+ * Created by archer on 17-4-7.
+ */
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * The type Jaxp ten.
+ */
+public class shiyan1_1 {
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
+    public static void main(String args[]) {
+        try {
+            String[] bookName = {"幽默大全", "时事评论", "感动中国"};
+            String[] bookVal = {"轻松幽默", "犀利独到", "耐人寻味"};
+            String[] bookScore = {"90","80","95"};
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder domPaser = factory.newDocumentBuilder();
+            Document document = domPaser.newDocument();
+            document.setXmlVersion("1.0");
+            Element root = document.createElement("喜欢的图书列表");
+            document.appendChild(root);
+            for (int k = 1; k <= bookName.length; k++) {
+                Node node = document.createElement("图书");
+                root.appendChild(node);
+            }
+            NodeList nodeList = document.getElementsByTagName("图书");
+            int size = nodeList.getLength();
+            for (int k = 0; k < size; k++) {
+                Node node = nodeList.item(k);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elementNode = (Element) node;
+                    Element nodeName = document.createElement("图书名");
+                    Element nodeVal = document.createElement("图书评价");
+                    nodeVal.setAttribute("图书质量",bookScore[k]);
+                    nodeName.appendChild(document.createTextNode(bookName[k]));
+                    nodeVal.appendChild(document.createTextNode(bookVal[k]));
+                    elementNode.appendChild(nodeName);
+                    elementNode.appendChild(nodeVal);
+                }
+            }
+            TransformerFactory transFactory = TransformerFactory.newInstance();
+            Transformer transformer = transFactory.newTransformer();
+            DOMSource domSource = new DOMSource(document);
+            File file = new File("src/shiyan/shiyan_xml/book.xml");
+            FileOutputStream out = new FileOutputStream(file);
+            StreamResult xmlResult = new StreamResult(out);
+            transformer.transform(domSource, xmlResult);
+            out.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/shiyan/shiyan1_2.java
@@ -0,0 +1,75 @@
+package shiyan;
+
+/**
+ * Created by archer on 17-4-7.
+ */
+
+import org.w3c.dom.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+/**
+ * The type Jaxp eight.
+ */
+public class shiyan1_2 {
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
+    public static void main(String args[]) {
+        GiveData give = new GiveData();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setIgnoringElementContentWhitespace(true); //忽略缩进空白。
+            DocumentBuilder domPaser = factory.newDocumentBuilder();
+            Document document = domPaser.parse(new File("src/shiyan/shiyan_xml/book.xml"));
+            NodeList nodeList = document.getChildNodes();
+            give.output(nodeList);
+            System.out.printf("\n一共有%d个Text节点", give.Text_count);
+            System.out.printf("\n一共有%d个Element节点",give.Element_count);
+        } catch (Exception e) {
+        }
+    }
+}
+
+/**
+ * The type Give data 8.
+ */
+class GiveData {
+    /**
+     * The Text_count.
+     */
+    int Text_count = 0;
+    /**
+     * The Element count.
+     */
+    int Element_count = 0;
+    /**
+     * Output.
+     *
+     * @param nodeList the node list
+     */
+    public void output(NodeList nodeList) {
+        int size = nodeList.getLength();
+        for (int k = 0; k < size; k++) {
+            Node node = nodeList.item(k);
+            if (node.getNodeType() == Node.TEXT_NODE) {
+                Text textNode = (Text) node;
+                String content = textNode.getWholeText();
+                Text_count++;
+                System.out.print(content);
+            }
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element elementNode = (Element) node;
+                String name = elementNode.getNodeName();
+                Element_count++;
+                System.out.println(name + ":");
+                NodeList nodes = elementNode.getChildNodes();
+                output(nodes);
+            }
+        }
+    }
+}
